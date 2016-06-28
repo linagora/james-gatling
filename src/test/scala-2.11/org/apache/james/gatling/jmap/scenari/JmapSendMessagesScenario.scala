ea18736f4b123cae8b9d3de38ec398262d5b1540
benwa <btellier@linagora.com>
2016-06-28 15:42:58 +0700

    ISSUE-6 I want to be able able to send an other user at random

---
+++ src/test/scala-2.11/org/apache/james/gatling/jmap/scenari/JmapSendMessagesScenario.scala
@@ -0,0 +1,25 @@
+package org.apache.james.gatling.jmap.scenari
+
+import io.gatling.core.Predef._
+import org.apache.james.gatling.control.UserFeeder
+import org.apache.james.gatling.jmap.scenari.common.CommonSteps
+import org.apache.james.gatling.jmap.{HttpSettings, JmapMessages}
+
+import scala.concurrent.duration._
+
+class JmapSendMessagesScenario extends Simulation {
+
+  val userCount = 100
+  val feeder = UserFeeder.createUserFeederWithInboxAndOutbox(userCount)
+  val loopVariableName = "any"
+
+  val scn = scenario("JmapSendMessages")
+    .exec(CommonSteps.provisionSystemMailboxes(userCount))
+    .repeat(360, loopVariableName) {
+      exec(JmapMessages.sendMessagesRandomly(feeder))
+        .pause(1 second , 2 seconds)
+    }
+
+  setUp(scn.inject(atOnceUsers(userCount))).protocols(HttpSettings.httpProtocol)
+
+}
