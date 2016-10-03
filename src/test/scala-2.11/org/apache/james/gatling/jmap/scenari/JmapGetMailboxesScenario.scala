75450e9407472b73ab7de7ff6460d28141ba9e2e
Raphael Ouazana <raphael.ouazana@linagora.com>
2016-10-03 15:25:42 +0200

    ISSUE-11 Automatic reauthentication for GetMailboxes scenario

--- src/test/scala-2.11/org/apache/james/gatling/jmap/scenari/JmapGetMailboxesScenario.scala
+++ src/test/scala-2.11/org/apache/james/gatling/jmap/scenari/JmapGetMailboxesScenario.scala
@@ -5,6 +5,7 @@ import org.apache.james.gatling.control.UserCreator
 import org.apache.james.gatling.jmap.JmapMailboxes
 import org.apache.james.gatling.jmap.scenari.common.Configuration._
 import org.apache.james.gatling.jmap.scenari.common._
+import org.apache.james.gatling.utils.RetryAuthentication._
 
 import scala.concurrent.duration._
 
@@ -13,7 +14,7 @@ class JmapGetMailboxesScenario extends Simulation {
   val scn = scenario("JmapGetMailboxes")
     .exec(CommonSteps.authentication(new UserCreator(BaseJamesWebAdministrationUrl).createUsersWithInboxAndOutbox(UserCount)))
     .during(ScenarioDuration) {
-      exec(JmapMailboxes.getSystemMailboxes)
+      execWithRetryAuthentication(JmapMailboxes.getSystemMailboxes, JmapMailboxes.getSystemMailboxesChecks)
         .pause(1 second , 2 seconds)
     }
 
