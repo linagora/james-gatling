28631c9827a847df936c2500da435b3bfe102420
benwa <btellier@linagora.com>
2016-10-14 17:07:20 +0200

    fixup! ISSUE-23 Implementation of the SMTP protocol

--- src/main/scala-2.11/org/apache/james/gatling/smtp/SmtpAction.scala
+++ src/main/scala-2.11/org/apache/james/gatling/smtp/SmtpAction.scala
@@ -20,45 +20,50 @@ class SmtpAction(
   val name = "sendMail"
 
   def execute(session: Session) {
-    def sessionVariable(variableName: String): String = {
-      session.apply(variableName).as[String]
-    }
-
+    val email: SimpleEmail = generateEmail(session)
     val reqStart = System.currentTimeMillis()
-    val email = new SimpleEmail()
-    var errorMessage: Option[String] = None
-    var status: Status = OK
-    val userEmail = sessionVariable(UserFeeder.UsernameSessionParam)
-
-    email.setHostName(protocol.host)
-    email.setSSLOnConnect(protocol.ssl)
-    email.setSmtpPort(protocol.port)
-
-    if (protocol.auth) {
-      email.setAuthentication(sessionVariable(UserFeeder.UsernameSessionParam), sessionVariable(UserFeeder.PasswordSessionParam))
-    }
-
-    email.setSubject(subject + " [" + Math.round(Math.random() * reqStart) + "]")
-    email.setMsg(body)
-    email.setFrom(userEmail)
-    email.addTo(userEmail)
-
-    try {
-      email.send()
-    } catch {
-      case e: Exception => {
-        logger.error("Exception caught while sending mail", e)
-
-        errorMessage = Some(e.toString)
-        status = KO
-      }
-    }
-
+    val executionReport = sendMail(email)
     val reqEnd = System.currentTimeMillis()
 
-    statsEngine.logResponse(session, requestName, ResponseTimings(reqStart, reqEnd), status, None, errorMessage)
+    statsEngine.logResponse(session, requestName, ResponseTimings(reqStart, reqEnd), executionReport.status, None, executionReport.errorMessage)
 
     next ! session
   }
 
+  private def generateEmail(session: Session): SimpleEmail = {
+    def sessionVariable(variableName: String): String = {
+      session.apply(variableName).as[String]
+    }
+
+    val email = new SimpleEmail()
+    val userEmail = sessionVariable(UserFeeder.UsernameSessionParam)
+    if (protocol.auth) {
+      email.setAuthentication(sessionVariable(UserFeeder.UsernameSessionParam), sessionVariable(UserFeeder.PasswordSessionParam))
+    }
+    email.setHostName(protocol.host)
+    email.setSSLOnConnect(protocol.ssl)
+    email.setSmtpPort(protocol.port)
+    email.setSubject(subject + " [" + Math.random() + "]")
+    email.setMsg(body)
+    email.setFrom(userEmail)
+    email.addTo(userEmail)
+    email
+  }
+
+  private def sendMail(email: SimpleEmail): ExecutionReport = {
+    try {
+      email.send()
+      GoodExecutionReport()
+    } catch {
+      case e: Exception => {
+        logger.error("Exception caught while sending mail", e)
+
+        BadExecutionReport(e.getMessage)
+      }
+    }
+  }
 }
+
+case class ExecutionReport(errorMessage: Option[String], status: Status)
+case class GoodExecutionReport() extends ExecutionReport(None, OK)
+case class BadExecutionReport(message: String) extends ExecutionReport(Some(message), KO)
