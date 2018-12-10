c37c800faff290f400a8c7b465174f11744ccdf4
Matthieu Baechler <matthieu.baechler@gmail.com>
2018-12-10 11:10:55 +0100

    use configuration ServerHostName for smtp protocol

--- src/test/scala-2.11/org/apache/james/gatling/smtp/scenari/common/SmtpSettings.scala
+++ src/test/scala-2.11/org/apache/james/gatling/smtp/scenari/common/SmtpSettings.scala
@@ -1,10 +1,11 @@
 package org.apache.james.gatling.smtp.scenari.common
 
+import org.apache.james.gatling.configuration.Configuration
 import org.apache.james.gatling.smtp.{SmtpActionBuilder, SmtpProtocol, SmtpProtocolBuilder}
 
 object SmtpSettings {
 
-  val smtp: SmtpProtocol = SmtpProtocolBuilder.default.build()
+  val smtp: SmtpProtocol = SmtpProtocolBuilder.default.host(Configuration.ServerHostName) build()
 
   def smtp(requestName: String) = SmtpActionBuilder(requestName, null, null)
 }
