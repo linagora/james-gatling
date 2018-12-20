31590008bf6947f118b77895fdc4dcdc298b2899
Gautier DI FOLCO <gdifolco@linagora.com>
2018-12-20 15:14:59 +0100

    WIP Use Jmap for provisionning Mailboxes

--- src/test/scala-2.11/org/apache/james/gatling/jmap/scenari/JmapGetMailboxesHugeNumberScenario.scala
+++ src/test/scala-2.11/org/apache/james/gatling/jmap/scenari/JmapGetMailboxesHugeNumberScenario.scala
@@ -9,9 +9,10 @@ import org.apache.james.gatling.jmap.scenari.common.{CommonSteps, HttpSettings}
 import scala.concurrent.duration._
 
 class JmapGetMailboxesHugeNumberScenario extends Simulation {
+  val users = new UserCreator(BaseJamesWebAdministrationUrl).createUsersWithInboxAndOutbox(UserCount)
 
   val scn = scenario("JmapGetMailboxesHugeNumber")
-    .exec(CommonSteps.authentication(new UserCreator(BaseJamesWebAdministrationUrl).createUsersWithInboxAndOutboxAndNumerousOthers(UserCount, 50)))
+    .exec(CommonSteps.provisionUsersWithMailboxesAndMessages(users, 2000, 0))
     .during(ScenarioDuration) {
       JmapMailboxes.getSystemMailboxesWithRetryAuthentication
         .pause(1 second , 2 seconds)
