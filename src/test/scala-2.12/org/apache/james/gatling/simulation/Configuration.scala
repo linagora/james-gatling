ffddfb6012e5b738d1465783142a45b6975883d3
Gautier DI FOLCO <gdifolco@linagora.com>
2019-05-14 11:58:14 +0200

    fixup! Parametrize ports and host with environment variables

--- src/test/scala-2.12/org/apache/james/gatling/simulation/Configuration.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/Configuration.scala
@@ -8,8 +8,12 @@ import scala.util.Properties
 object Configuration {
 
   val ServerHostName = Properties.envOrElse("TARGET_HOSTNAME", "localhost")
-  val BaseJmapUrl = s"http://$ServerHostName:${Properties.envOrElse("JMAP_PORT", "1080")}"
-  val BaseJamesWebAdministrationUrl = new URL(s"http://$ServerHostName:${Properties.envOrElse("WEBADMIN_PORT", "8000")}")
+
+  val JMAP_PORT = Properties.envOrElse("JMAP_PORT", "1080").toInt
+  val BaseJmapUrl = s"http://$ServerHostName:$JMAP_PORT"
+
+  val WEBADMIN_PORT = Properties.envOrElse("WEBADMIN_PORT", "8000").toInt
+  val BaseJamesWebAdministrationUrl = new URL(s"http://$ServerHostName:$WEBADMIN_PORT")
 
   val ScenarioDuration = 3 hours
   val UserCount = 1000
