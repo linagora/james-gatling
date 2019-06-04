d40f98d53807ac1d61ceeb120981558d15d5879d
Rémi Kowalski <rkowalski@linagora.com>
2019-06-04 16:58:56 +0200

    fix ImapAuthenticationScenario

--- src/it/scala-2.12/org/apache/james/gatling/ImapAuthenticationIT.scala
+++ src/it/scala-2.12/org/apache/james/gatling/ImapAuthenticationIT.scala
@@ -1,15 +1,11 @@
 package org.apache.james.gatling
 
 import org.apache.james.gatling.imap.scenari.ImapAuthenticationScenario
+
 import scala.concurrent.duration._
 
 
 class ImapAuthenticationIT extends ImapIT {
-
-  before {
-    users.foreach(server.sendMessage(Fixture.homer.username))
-  }
-
   scenario(feederBuilder => {
     new ImapAuthenticationScenario().generate(10 seconds, feederBuilder)
   })
