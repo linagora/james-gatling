d513d67150fab9027eb55ce655bdb398ce902064
Raphael Ouazana <raphael.ouazana@linagora.com>
2019-06-13 12:02:53 +0200

    Allow to configure different hosts for different services

--- src/test/scala-2.12/org/apache/james/gatling/simulation/Configuration.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/Configuration.scala
@@ -8,14 +8,17 @@ import scala.util.Properties
 object Configuration {
 
   val ServerHostName = Properties.envOrElse("TARGET_HOSTNAME", "localhost")
+  val JmapServerHostName = Properties.envOrElse("JMAP_TARGET_HOSTNAME", ServerHostName)
+  val ImapServerHostName = Properties.envOrElse("IMAP_SERVER_HOSTNAME", ServerHostName)
+  val WebadminServerHostName = Properties.envOrElse("WEBADMIN_SERVER_HOSTNAME", ServerHostName)
 
   val JMAP_PORT = Properties.envOrElse("JMAP_PORT", "1080").toInt
   val JMAP_PROTOCOL = Properties.envOrElse("JMAP_PROTOCOL", "http")
-  val BaseJmapUrl = s"$JMAP_PROTOCOL://$ServerHostName:$JMAP_PORT"
+  val BaseJmapUrl = s"$JMAP_PROTOCOL://$JmapServerHostName:$JMAP_PORT"
 
   val WEBADMIN_PORT = Properties.envOrElse("WEBADMIN_PORT", "8000").toInt
   val WEBADMIN_PROTOCOL = Properties.envOrElse("WEBADMIN_PROTOCOL", "http")
-  val BaseJamesWebAdministrationUrl = new URL(s"$WEBADMIN_PROTOCOL://$ServerHostName:$WEBADMIN_PORT")
+  val BaseJamesWebAdministrationUrl = new URL(s"$WEBADMIN_PROTOCOL://$WebadminServerHostName:$WEBADMIN_PORT")
 
   val ScenarioDuration = 3 hours
   val UserCount = 1000
