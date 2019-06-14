72a10fcee09c6fbfdd07adfe20dcee4f372b94e1
Raphael Ouazana <raphael.ouazana@linagora.com>
2019-06-14 11:18:06 +0200

    Remove useless FeederNoAuthenticationNoEncryptionScenario

--- src/test/scala-2.12/org/apache/james/gatling/simulation/smtp/FeederNoAuthenticationNoEncryptionSimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/smtp/FeederNoAuthenticationNoEncryptionSimulation.scala
@@ -3,7 +3,7 @@ package org.apache.james.gatling.simulation.smtp
 import io.gatling.core.Predef._
 import org.apache.james.gatling.simulation.Configuration
 import org.apache.james.gatling.smtp.SmtpProtocol.smtp
-import org.apache.james.gatling.smtp.scenari.FeederNoAuthenticationNoEncryptionScenario
+import org.apache.james.gatling.smtp.scenari.SmtpNoAuthenticationNoEncryptionScenario
 
 import scala.concurrent.duration._
 
@@ -11,7 +11,7 @@ class FeederNoAuthenticationNoEncryptionSimulation extends Simulation {
 
   private val feeder = csv("users.csv")
 
-  private val scenario = new FeederNoAuthenticationNoEncryptionScenario()
+  private val scenario = new SmtpNoAuthenticationNoEncryptionScenario()
 
   setUp(scenario.generate(Configuration.ScenarioDuration, feeder.circular)
     .inject(nothingFor(10 seconds), rampUsers(Configuration.UserCount) during(10 seconds)))
