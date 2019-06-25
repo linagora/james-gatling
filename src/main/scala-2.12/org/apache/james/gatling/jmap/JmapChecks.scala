60d7f4c297fd7fe4e8b6a4805ad444d7d73de289
Rémi Kowalski <rkowalski@linagora.com>
2019-06-25 11:17:46 +0200

    Fix noError Jmap check

--- src/main/scala-2.12/org/apache/james/gatling/jmap/JmapChecks.scala
+++ src/main/scala-2.12/org/apache/james/gatling/jmap/JmapChecks.scala
@@ -1,10 +1,14 @@
 package org.apache.james.gatling.jmap
 
 import io.gatling.core.Predef._
+import io.gatling.core.check.extractor.jsonpath.{JsonPathCheckBuilder, JsonPathOfType}
 
 object JmapChecks {
 
-  val noError = jsonPath("$.error").notExists
+  private val hasErrorPath: JsonPathCheckBuilder[String] with JsonPathOfType = jsonPath("$[?(@[0] == 'error')]")
+
+  val noError = hasErrorPath.notExists
+  val hasError = hasErrorPath.exists
 
   def created() = jsonPath(s"$$[0][1].created['$${${JmapMessages.messageIdSessionParam}}'].id").exists
 
