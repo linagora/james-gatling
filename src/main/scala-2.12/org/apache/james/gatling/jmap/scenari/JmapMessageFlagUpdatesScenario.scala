3c9d94e274b399cd821a9c1715c32cf9d18b1798
Rene Cordier <rcordier@linagora.com>
2019-10-15 11:34:46 +0700

    fixup! JAMES-2906 Add a message flag updates scenario for a simulation on mail corpus

--- src/main/scala-2.12/org/apache/james/gatling/jmap/scenari/JmapMessageFlagUpdatesScenario.scala
+++ src/main/scala-2.12/org/apache/james/gatling/jmap/scenari/JmapMessageFlagUpdatesScenario.scala
@@ -3,7 +3,8 @@ package org.apache.james.gatling.jmap.scenari
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 import org.apache.james.gatling.control.UserFeeder.UserFeederBuilder
-import org.apache.james.gatling.jmap.{CommonSteps, JmapMessages}
+import org.apache.james.gatling.jmap.JmapMessages.openpaasListMessageParameters
+import org.apache.james.gatling.jmap.{CommonSteps, JmapMailbox, JmapMessages, RetryAuthentication}
 
 class JmapMessageFlagUpdatesScenario {
   private object Keys {
@@ -17,10 +18,12 @@ class JmapMessageFlagUpdatesScenario {
       .feed(userFeeder)
       .exec(CommonSteps.authentication())
       .group(MessageFlagUpdates.name)(
-        randomSwitch(
-          70.0 -> exec(JmapMessages.markAsRead()),
-          20.0 -> exec(JmapMessages.markAsAnswered()),
-          10.0 -> exec(JmapMessages.markAsFlagged())
-        ))
+        exec(RetryAuthentication.execWithRetryAuthentication(JmapMailbox.getMailboxes, JmapMailbox.getMailboxesChecks ++ JmapMailbox.saveInboxAs(Keys.inbox)))
+          .exec(RetryAuthentication.execWithRetryAuthentication(JmapMessages.listMessages(openpaasListMessageParameters(Keys.inbox)), JmapMessages.listMessagesChecks))
+          .randomSwitch(
+            70.0 -> exec(JmapMessages.markAsRead()),
+            20.0 -> exec(JmapMessages.markAsAnswered()),
+            10.0 -> exec(JmapMessages.markAsFlagged())
+          ))
   }
 }
