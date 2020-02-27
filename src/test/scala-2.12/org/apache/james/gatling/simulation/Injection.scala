7781acc3a689d0dac81197f3868fc06cc8d500ce
Gautier DI FOLCO <gdifolco@linagora.com>
2020-02-27 12:50:59 +0100

    fixup! Add mixed body size SMTP Simulation

--- src/test/scala-2.12/org/apache/james/gatling/simulation/Injection.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/Injection.scala
@@ -5,11 +5,13 @@ import io.gatling.core.Predef.constantUsersPerSec
 
 import scala.concurrent.duration._
 
-object Injection {
-
+abstract sealed class UsersDensity {
+  def constantUserPerHour: Predef.ConstantRateBuilder
+}
+case class UsersPerHour(nb: Double) extends UsersDensity {
   private def toUserPerSec(userPerHour: Double): Double = userPerHour / 1.hour.toSeconds
-  def toUsersPerHour(userPerSecond: Double): Double = userPerSecond * 1.hour.toSeconds
-
-  def constantUserPerHour(nb: Double): Predef.ConstantRateBuilder = constantUsersPerSec(toUserPerSec(nb))
-
+  override def constantUserPerHour: Predef.ConstantRateBuilder = constantUsersPerSec(toUserPerSec(nb))
+}
+case class UsersPerSecond(nb: Double) extends UsersDensity {
+  override def constantUserPerHour: Predef.ConstantRateBuilder = constantUsersPerSec(nb)
 }
--- src/test/scala-2.12/org/apache/james/gatling/simulation/smtp/SmtpNoAuthenticationNoEncryptionMixedBodySimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/smtp/SmtpNoAuthenticationNoEncryptionMixedBodySimulation.scala
@@ -1,15 +1,15 @@
 package org.apache.james.gatling.simulation.smtp
 
 import org.apache.james.gatling.jmap.scenari.InboxHomeLoading
-import org.apache.james.gatling.simulation.{Configuration, Injection, SimulationOnMailCorpus}
+import org.apache.james.gatling.simulation.{Configuration, UsersPerSecond, SimulationOnMailCorpus}
 import org.apache.james.gatling.smtp.scenari.{SmtpNoAuthenticationNoEncryptionBigBodyScenario, SmtpNoAuthenticationNoEncryptionScenario}
 
 import io.gatling.core.Predef._
 
 import scala.concurrent.duration._
 
-class SmtpNoAuthenticationNoEncryptionMixtedBodySimulation extends Simulation with SimulationOnMailCorpus {
-  private val concurrentUsers = Injection.toUsersPerHour(Configuration.UserCount / 2).toInt
+class SmtpNoAuthenticationNoEncryptionMixedBodySimulation extends Simulation with SimulationOnMailCorpus {
+  private val concurrentUsers = UsersPerSecond(Configuration.UserCount / 2)
   private val scenarioSmallBody = new SmtpNoAuthenticationNoEncryptionScenario()
   private val scenarioBigBody = new SmtpNoAuthenticationNoEncryptionBigBodyScenario()
 
