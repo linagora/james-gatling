0c7f0cdca4cff910ef6f028c79d4438158c5606a
Gautier DI FOLCO <gdifolco@linagora.com>
2020-02-28 08:42:52 +0100

    Drop IMAP assertions in SMTP simulations

--- src/test/scala-2.12/org/apache/james/gatling/simulation/smtp/SmtpNoAuthenticationNoEncryptionMixedBodySimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/smtp/SmtpNoAuthenticationNoEncryptionMixedBodySimulation.scala
@@ -1,12 +1,9 @@
 package org.apache.james.gatling.simulation.smtp
 
-import org.apache.james.gatling.jmap.scenari.InboxHomeLoading
 import org.apache.james.gatling.simulation.{Configuration, UsersPerSecond, SimulationOnMailCorpus}
 import org.apache.james.gatling.smtp.scenari.{SmtpNoAuthenticationNoEncryptionBigBodyScenario, SmtpNoAuthenticationNoEncryptionScenario}
 
 import io.gatling.core.Predef._
-
-import scala.concurrent.duration._
 
 class SmtpNoAuthenticationNoEncryptionMixedBodySimulation extends Simulation with SimulationOnMailCorpus {
   private val concurrentUsers = UsersPerSecond(Configuration.UserCount / 2)
@@ -15,6 +12,5 @@ class SmtpNoAuthenticationNoEncryptionMixedBodySimulation extends Simulation wit
 
   setUp(injectUsersInScenario(scenarioSmallBody.generate(Configuration.ScenarioDuration, feeder), concurrentUsers),
         injectUsersInScenario(scenarioBigBody.generate(Configuration.ScenarioDuration, feeder), concurrentUsers))
-    .assertions(buildMaxScenarioResponseTimeAssertion(InboxHomeLoading, 2 seconds))
 }
 
