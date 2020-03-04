82e0d1718aa060462573e071072842515717bf63
Gautier DI FOLCO <gdifolco@linagora.com>
2020-03-04 11:13:23 +0100

    Inject IMAP protocol in simulations

--- src/test/scala-2.12/org/apache/james/gatling/simulation/imap/ImapListMessagesBodyStructureSimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/imap/ImapListMessagesBodyStructureSimulation.scala
@@ -3,12 +3,15 @@ package org.apache.james.gatling.simulation.imap
 import io.gatling.core.scenario.Simulation
 import org.apache.james.gatling.imap.scenari.ImapListMessagesBodyStructureScenario
 import org.apache.james.gatling.jmap.scenari.ListMessageBodyStructure
-import org.apache.james.gatling.simulation.SimulationOnMailCorpus
+import org.apache.james.gatling.simulation.{Configuration, HttpSettings, SimulationOnMailCorpus}
+
+import com.linagora.gatling.imap.PreDef.imap
 
 import scala.concurrent.duration._
 
 class ImapListMessagesBodyStructureSimulation extends Simulation with SimulationOnMailCorpus {
   private val MAILS_FECTHED = 200
   setUp(injectUsersInScenario(new ImapListMessagesBodyStructureScenario().generate(feeder, getMailboxes, MAILS_FECTHED)))
+    .protocols(HttpSettings.httpProtocol, imap.host(Configuration.ImapServerHostName).build())
     .assertions(buildMaxScenarioResponseTimeAssertion(ListMessageBodyStructure, 2 seconds))
 }
--- src/test/scala-2.12/org/apache/james/gatling/simulation/imap/ImapStoreSimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/imap/ImapStoreSimulation.scala
@@ -3,11 +3,14 @@ package org.apache.james.gatling.simulation.imap
 import io.gatling.core.scenario.Simulation
 import org.apache.james.gatling.imap.scenari.ImapStoreScenario
 import org.apache.james.gatling.jmap.scenari.InboxHomeLoading
-import org.apache.james.gatling.simulation.{Configuration, SimulationOnMailCorpus}
+import org.apache.james.gatling.simulation.{Configuration, HttpSettings, SimulationOnMailCorpus}
+
+import com.linagora.gatling.imap.PreDef.imap
 
 import scala.concurrent.duration._
 
 class ImapStoreSimulation extends Simulation with SimulationOnMailCorpus {
   setUp(injectUsersInScenario(new ImapStoreScenario().generate(Configuration.ScenarioDuration, feeder)))
+    .protocols(HttpSettings.httpProtocol, imap.host(Configuration.ImapServerHostName).build())
     .assertions(buildMaxScenarioResponseTimeAssertion(InboxHomeLoading, 2 seconds))
 }
