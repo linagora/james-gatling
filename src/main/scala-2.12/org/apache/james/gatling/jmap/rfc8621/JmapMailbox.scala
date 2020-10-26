a638d2ae335dd70b4d73d3ef042beb97d7d2ed63
Rene Cordier <rcordier@linagora.com>
2020-10-26 11:42:47 +0700

    ISSUE-84 Mailbox/get step scenario & simulations

--- src/main/scala-2.12/org/apache/james/gatling/jmap/rfc8621/JmapMailbox.scala
+++ src/main/scala-2.12/org/apache/james/gatling/jmap/rfc8621/JmapMailbox.scala
@@ -1,5 +1,25 @@
 package org.apache.james.gatling.jmap.rfc8621
 
-class JmapMailbox {
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef.http
 
+object JmapMailbox {
+  def getMailboxes: ChainBuilder =
+    exec(
+      http("getMailboxes")
+        .post("/jmap")
+        .headers(JmapHttp.HEADERS_JSON)
+        .basicAuth("${username}", "${password}")
+      .body(StringBody(
+        s"""{
+           |  "using": ["urn:ietf:params:jmap:core","urn:ietf:params:jmap:mail"],
+           |  "methodCalls": [[
+           |    "Mailbox/get",
+           |    {
+           |      "accountId": "$${accountId}",
+           |      "ids": null
+           |    },
+           |    "c1"]]
+           |}""".stripMargin)))
 }
--- src/main/scala-2.12/org/apache/james/gatling/jmap/rfc8621/scenari/MailboxGetScenario.scala
+++ src/main/scala-2.12/org/apache/james/gatling/jmap/rfc8621/scenari/MailboxGetScenario.scala
@@ -3,7 +3,7 @@ package org.apache.james.gatling.jmap.rfc8621.scenari
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 import org.apache.james.gatling.control.UserFeeder.UserFeederBuilder
-import org.apache.james.gatling.jmap.rfc8621.SessionStep
+import org.apache.james.gatling.jmap.rfc8621.{JmapMailbox, SessionStep}
 
 import scala.concurrent.duration._
 
@@ -11,9 +11,9 @@ class MailboxGetScenario {
   def generate(duration: Duration, userFeeder: UserFeederBuilder): ScenarioBuilder =
     scenario("MailboxGetScenario")
       .feed(userFeeder)
-      .exec(SessionStep.retrieveAccountId())
+      .exec(SessionStep.retrieveAccountId)
       .during(duration) {
-        exec(SessionStep.retrieveAccountId())
-          .pause(1 second)
+        exec(JmapMailbox.getMailboxes
+          .pause(1 second))
       }
 }
