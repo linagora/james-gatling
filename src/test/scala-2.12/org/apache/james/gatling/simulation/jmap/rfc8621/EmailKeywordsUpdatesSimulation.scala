f15d8bc5f59aa95cc43208364c1eb8164da84e60
Rene Cordier <rcordier@linagora.com>
2020-11-06 17:14:03 +0700

    ISSUE-87 EmailKeywordsUpdatesSimulation

---
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/rfc8621/EmailKeywordsUpdatesSimulation.scala
@@ -0,0 +1,13 @@
+package org.apache.james.gatling.simulation.jmap.rfc8621
+
+import io.gatling.core.scenario.Simulation
+import org.apache.james.gatling.jmap.MessageFlagUpdates
+import org.apache.james.gatling.jmap.rfc8621.scenari.EmailKeywordsUpdatesScenario
+import org.apache.james.gatling.simulation.SimulationOnMailCorpus
+
+import scala.concurrent.duration.DurationInt
+
+class EmailKeywordsUpdatesSimulation extends Simulation with SimulationOnMailCorpus {
+  setUp(injectUsersInScenario(new EmailKeywordsUpdatesScenario().generate(feeder)))
+    .assertions(buildMaxScenarioResponseTimeAssertion(MessageFlagUpdates, 2 seconds))
+}
