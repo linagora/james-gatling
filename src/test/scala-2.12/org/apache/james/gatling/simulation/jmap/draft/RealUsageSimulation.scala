a3c7bc3685c51893b8827c711b9f158fe80f1352
Benoit Tellier <btellier@linagora.com>
2020-11-22 09:02:35 +0700

    => 600

--- src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/draft/RealUsageSimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/draft/RealUsageSimulation.scala
@@ -3,16 +3,16 @@ package org.apache.james.gatling.simulation.jmap.draft
 import io.gatling.core.scenario.Simulation
 import org.apache.james.gatling.jmap.{InboxHomeLoading, OpenMessage, SelectMailbox}
 import org.apache.james.gatling.jmap.draft.scenari.{JmapInboxHomeLoadingScenario, JmapOpenArbitraryMessageScenario, JmapSelectArbitraryMailboxScenario}
-import org.apache.james.gatling.simulation.SimulationOnMailCorpus
+import org.apache.james.gatling.simulation.{SimulationOnMailCorpus, UsersPerHour}
 
 import scala.concurrent.duration._
 
 class RealUsageSimulation extends Simulation with SimulationOnMailCorpus {
   private val MIN_MESSAGES_IN_MAILBOXES_TO_SELECT = 100
   setUp(
-    injectUsersInScenario(new JmapInboxHomeLoadingScenario().generate(feeder)),
-    injectUsersInScenario(new JmapOpenArbitraryMessageScenario().generate(feeder)),
-    injectUsersInScenario(new JmapSelectArbitraryMailboxScenario(MIN_MESSAGES_IN_MAILBOXES_TO_SELECT).generate(feeder))
+    injectUsersInScenario(new JmapInboxHomeLoadingScenario().generate(feeder), UsersPerHour(600)),
+    injectUsersInScenario(new JmapOpenArbitraryMessageScenario().generate(feeder), UsersPerHour(600)),
+    injectUsersInScenario(new JmapSelectArbitraryMailboxScenario(MIN_MESSAGES_IN_MAILBOXES_TO_SELECT).generate(feeder), UsersPerHour(600))
   ).assertions(
     buildMaxScenarioResponseTimeAssertion(InboxHomeLoading, 2 seconds),
     buildMaxScenarioResponseTimeAssertion(OpenMessage, 500 millis),
--- src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/rfc8621/RealUsageSimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/rfc8621/RealUsageSimulation.scala
@@ -3,7 +3,7 @@ package org.apache.james.gatling.simulation.jmap.rfc8621
 import io.gatling.core.scenario.Simulation
 import org.apache.james.gatling.jmap.rfc8621.scenari.{InboxLoadingScenario, OpenEmailScenario, SelectMailboxScenario}
 import org.apache.james.gatling.jmap.{InboxHomeLoading, OpenMessage, SelectMailbox}
-import org.apache.james.gatling.simulation.SimulationOnMailCorpus
+import org.apache.james.gatling.simulation.{SimulationOnMailCorpus, UsersPerHour}
 
 import scala.concurrent.duration._
 
@@ -11,9 +11,9 @@ class RealUsageSimulation extends Simulation with SimulationOnMailCorpus {
   private val MIN_MESSAGES_IN_MAILBOXES_TO_SELECT = 100
 
   setUp(
-      injectUsersInScenario(new InboxLoadingScenario().generate(feeder)),
-      injectUsersInScenario(new OpenEmailScenario().generate(feeder)),
-      injectUsersInScenario(new SelectMailboxScenario(MIN_MESSAGES_IN_MAILBOXES_TO_SELECT).generate(feeder)))
+      injectUsersInScenario(new InboxLoadingScenario().generate(feeder), UsersPerHour(600)),
+      injectUsersInScenario(new OpenEmailScenario().generate(feeder), UsersPerHour(600)),
+      injectUsersInScenario(new SelectMailboxScenario(MIN_MESSAGES_IN_MAILBOXES_TO_SELECT).generate(feeder), UsersPerHour(600)))
   .assertions(
       buildMaxScenarioResponseTimeAssertion(InboxHomeLoading, 2 seconds),
       buildMaxScenarioResponseTimeAssertion(OpenMessage, 1 second),
