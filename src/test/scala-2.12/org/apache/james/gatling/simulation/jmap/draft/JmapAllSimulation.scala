dfb8730782b0b5eeb8efb2338445c4e39ff16513
LanKhuat <dlkhuat@linagora.com>
2020-11-25 14:33:43 +0700

    ISSUE-89 Update draft AllSimulation

--- src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/draft/JmapAllSimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/draft/JmapAllSimulation.scala
@@ -2,24 +2,34 @@ package org.apache.james.gatling.simulation.jmap.draft
 
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
-import org.apache.james.gatling.control.{RecipientFeeder, UserCreator, UserFeeder}
-import org.apache.james.gatling.jmap.draft.scenari.JmapAllScenario
-import org.apache.james.gatling.simulation.{Configuration, HttpSettings}
+import io.gatling.core.structure.ScenarioBuilder
+import org.apache.james.gatling.control.RecipientFeeder
+import org.apache.james.gatling.control.RecipientFeeder.RecipientFeederBuilder
+import org.apache.james.gatling.control.UserFeeder.UserFeederBuilder
+import org.apache.james.gatling.jmap.draft.scenari.{JmapInboxHomeLoadingScenario, JmapMessageFlagUpdatesScenario, JmapOpenArbitraryMessageScenario, JmapSelectArbitraryMailboxScenario}
+import org.apache.james.gatling.jmap.draft.{CommonSteps, JmapMessages}
+import org.apache.james.gatling.jmap.{InboxHomeLoading, OpenMessage, SelectMailbox}
+import org.apache.james.gatling.simulation.{SimulationOnMailCorpus, UsersPerSecond}
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration.Inf
-import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 
-class JmapAllSimulation extends Simulation {
+class JmapAllSimulation extends Simulation with SimulationOnMailCorpus {
+  private val MIN_MESSAGES_IN_MAILBOXES_TO_SELECT = 100
 
-  private val users = Await.result(
-      awaitable = Future.sequence(
-        new UserCreator(Configuration.BaseJamesWebAdministrationUrl, Configuration.BaseJmapUrl).createUsersWithInboxAndOutbox(Configuration.UserCount)),
-      atMost = Inf)
+  def sendMessageScenario(userFeeder: UserFeederBuilder, recipientFeeder: RecipientFeederBuilder): ScenarioBuilder =
+    scenario("JmapSendMessages")
+      .feed(userFeeder)
+      .exec(CommonSteps.provisionSystemMailboxes())
+      .exec(JmapMessages.sendMessagesToUserWithRetryAuthentication(recipientFeeder))
 
-  private val scenario = new JmapAllScenario()
-
-  setUp(scenario.generate(UserFeeder.toFeeder(users), Configuration.ScenarioDuration, RecipientFeeder.usersToFeeder(users))
-      .inject(atOnceUsers(Configuration.UserCount)))
-    .protocols(HttpSettings.httpProtocol)
+  setUp(
+      injectUsersInScenario(new JmapInboxHomeLoadingScenario().generate(feeder), UsersPerSecond(0.5)),
+      injectUsersInScenario(new JmapOpenArbitraryMessageScenario().generate(feeder), UsersPerSecond(0.5)),
+      injectUsersInScenario(new JmapSelectArbitraryMailboxScenario(MIN_MESSAGES_IN_MAILBOXES_TO_SELECT).generate(feeder), UsersPerSecond(0.5)),
+      injectUsersInScenario(new JmapMessageFlagUpdatesScenario().generate(feeder), UsersPerSecond(0.5)),
+      injectUsersInScenario(sendMessageScenario(feeder, RecipientFeeder.usersToFeeder(getUsers)), UsersPerSecond(0.5)))
+    .assertions(
+      buildMaxScenarioResponseTimeAssertion(InboxHomeLoading, 2 seconds),
+      buildMaxScenarioResponseTimeAssertion(OpenMessage, 1 second),
+      buildMaxScenarioResponseTimeAssertion(SelectMailbox, 2 second))
 }
