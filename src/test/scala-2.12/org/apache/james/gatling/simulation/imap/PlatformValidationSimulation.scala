aa8d9b2fe7438112402ff2ae8cbb5897e494efce
Benoit Tellier <btellier@linagora.com>
2020-11-27 10:16:10 +0100

    Use feeders for platform validation

--- src/test/scala-2.12/org/apache/james/gatling/simulation/imap/PlatformValidationSimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/imap/PlatformValidationSimulation.scala
@@ -1,13 +1,29 @@
 package org.apache.james.gatling.simulation.imap
 
+import com.linagora.gatling.imap.PreDef.imap
 import io.gatling.core.Predef._
+import org.apache.james.gatling.control.UserFeeder.UserFeeder
+import org.apache.james.gatling.control.{Password, User, UserFeeder, Username}
 import org.apache.james.gatling.imap.scenari.PlatformValidationScenario
 import org.apache.james.gatling.simulation.Configuration.{InjectionDuration, ScenarioDuration, UserCount}
-import org.apache.james.gatling.simulation.{HttpSettings, SimulationOnMailCorpus}
+import org.apache.james.gatling.simulation.{Configuration, HttpSettings}
 
-class PlatformValidationSimulation extends Simulation with SimulationOnMailCorpus {
+class PlatformValidationSimulation extends Simulation {
+  private def recordValueToString(recordValue: Any): String = recordValue match {
+    case s: String => s
+    case a: Any => println("Warning: calling toString on a feeder value"); a.toString
+  }
+
+  val authenticatedUsers: Seq[User] = csv("users.csv").readRecords
+    .map(record =>
+      User(
+        username = Username(recordValueToString(record("username"))),
+        password = Password(recordValueToString(record("password")))))
+
+  val feeder: UserFeeder = UserFeeder.toFeeder(authenticatedUsers)
+
   setUp(new PlatformValidationScenario()
       .generate(duration = ScenarioDuration, userFeeder = feeder)
     .inject(rampUsers(UserCount) during InjectionDuration)
-    .protocols(HttpSettings.httpProtocol))
+    .protocols(HttpSettings.httpProtocol, imap.host(Configuration.ImapServerHostName).build()))
 }
--- src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/draft/PlatformValidationSimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/draft/PlatformValidationSimulation.scala
@@ -1,14 +1,28 @@
 package org.apache.james.gatling.simulation.jmap.draft
 
 import io.gatling.core.Predef._
-import org.apache.james.gatling.control.RecipientFeeder
+import org.apache.james.gatling.control.AuthenticatedUserFeeder.AuthenticatedUserFeederBuilder
+import org.apache.james.gatling.control.{Password, RecipientFeeder, User, UserFeeder, Username}
 import org.apache.james.gatling.jmap.draft.scenari.PlatformValidationScenario
 import org.apache.james.gatling.simulation.Configuration.{InjectionDuration, ScenarioDuration, UserCount}
-import org.apache.james.gatling.simulation.{HttpSettings, SimulationOnMailCorpus}
+import org.apache.james.gatling.simulation.HttpSettings
 
-class PlatformValidationSimulation extends Simulation with SimulationOnMailCorpus {
+class PlatformValidationSimulation extends Simulation {
+  private def recordValueToString(recordValue: Any): String = recordValue match {
+    case s: String => s
+    case a: Any => println("Warning: calling toString on a feeder value"); a.toString
+  }
+
+  val authenticatedUsers: Seq[User] = csv("users.csv").readRecords
+    .map(record =>
+      User(
+        username = Username(recordValueToString(record("username"))),
+        password = Password(recordValueToString(record("password")))))
+
+  val feeder: AuthenticatedUserFeederBuilder = UserFeeder.toFeeder(authenticatedUsers)
+
   setUp(new PlatformValidationScenario(minMessagesInMailbox = 10)
-      .generate(duration = ScenarioDuration, userFeeder = feeder, recipientFeeder = RecipientFeeder.usersToFeeder(getUsers))
+      .generate(duration = ScenarioDuration, userFeeder = feeder, recipientFeeder = RecipientFeeder.usersToFeeder(authenticatedUsers))
     .inject(rampUsers(UserCount) during InjectionDuration)
     .protocols(HttpSettings.httpProtocol))
 }
