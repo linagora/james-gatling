9c0bbe606f63f380035d8ef42ae56cad675fc278
Tellier Benoit <btellier@linagora.com>
2021-05-17 14:57:24 +0700

    ISSUE-111 Lean prepare step for PUSH prepare (#123)
    Triggers 4 getMailboxes calls, one is enough

--- src/main/scala-2.12/org/apache/james/gatling/jmap/scenari/PushPlatformValidationScenario.scala
+++ src/main/scala-2.12/org/apache/james/gatling/jmap/scenari/PushPlatformValidationScenario.scala
@@ -5,13 +5,14 @@ import io.gatling.core.structure.{ChainBuilder, ScenarioBuilder}
 import io.gatling.http.Predef._
 import org.apache.james.gatling.control.RecipientFeeder.RecipientFeederBuilder
 import org.apache.james.gatling.control.UserFeeder.UserFeederBuilder
+import org.apache.james.gatling.jmap.draft.JmapMessages.openpaasListMessageParameters
 import org.apache.james.gatling.jmap.draft.scenari.{JmapInboxHomeLoadingScenario, JmapOpenArbitraryMessageScenario, JmapSelectArbitraryMailboxScenario}
 import org.apache.james.gatling.jmap.draft.{CommonSteps, JmapMessages}
 import org.apache.james.gatling.jmap.rfc8621.JmapEmail.{nonEmptyListMessagesChecks, openpaasEmailQueryParameters, queryEmails}
 import org.apache.james.gatling.jmap.rfc8621.JmapHttp.{noError, statusOk}
 import org.apache.james.gatling.jmap.rfc8621.JmapWebsocket.{enablePush, websocketClose, websocketConnect}
 import org.apache.james.gatling.jmap.rfc8621.{JmapEmail, JmapMailbox, SessionStep}
-import org.apache.james.gatling.jmap.scenari.PushPlatformValidationScenario.{draft, emailIds, emailState, inbox, mailboxState, outbox}
+import org.apache.james.gatling.jmap.scenari.PushPlatformValidationScenario.{draft, emailIds, emailState, inbox, mailboxState, messageIds, outbox, randomMailbox}
 
 import scala.concurrent.duration._
 
@@ -23,6 +24,8 @@ object PushPlatformValidationScenario {
   val outbox = "outboxMailboxId"
   val accountId = "accountId"
   val emailIds = "emailIds"
+  val messageIds = "messageIds"
+  val randomMailbox = "randomMailbox"
 }
 
 class PushPlatformValidationScenario(minMessagesInMailbox: Int,
@@ -52,14 +55,20 @@ class PushPlatformValidationScenario(minMessagesInMailbox: Int,
       .group("prepare")(
         exec(SessionStep.retrieveAccountId)
         .exec(JmapMailbox.getMailboxes
-          .check(statusOk, noError, JmapMailbox.saveStateAs(mailboxState), JmapMailbox.saveInboxAs(inbox), JmapMailbox.saveDraftAs(draft), JmapMailbox.saveOutboxAs(outbox)))
+          .check(statusOk, noError,
+            JmapMailbox.saveStateAs(mailboxState),
+            JmapMailbox.saveInboxAs(inbox),
+            JmapMailbox.saveDraftAs(draft),
+            JmapMailbox.saveOutboxAs(outbox),
+            JmapMailbox.saveRandomMailboxWithAtLeastMessagesAs(randomMailbox, minMessagesInMailbox)))
         .exec(queryEmails(queryParameters = openpaasEmailQueryParameters(inbox))
-          .check(statusOk, noError, nonEmptyListMessagesChecks(emailIds)))
+          .check(statusOk, noError,
+            nonEmptyListMessagesChecks(emailIds),
+            nonEmptyListMessagesChecks(messageIds)))
         .exec(JmapEmail.getState()
           .check(statusOk, noError, JmapEmail.saveStateAs(emailState)))
         .exec(CommonSteps.authentication())
-        .exec(openArbitrary.prepare)
-        .exec(selectArbitrary.prepare))
+        .exec(JmapMessages.listMessages(openpaasListMessageParameters(inbox))))
       .exec(websocketConnect().onConnected(
         exec(enablePush)
           .during(duration) {
