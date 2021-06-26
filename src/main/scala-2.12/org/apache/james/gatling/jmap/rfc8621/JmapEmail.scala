47527aa5f3287202ad3a9b248ed860fd00d8b9d0
Benoit Tellier <btellier@linagora.com>
2021-06-26 19:59:56 +0700

    ISSUE-110 Update tracked state for /changes
    The initial state was used, which results in
    longer and longer calls as the simulation
    goes on.

--- src/main/scala-2.12/org/apache/james/gatling/jmap/rfc8621/JmapEmail.scala
+++ src/main/scala-2.12/org/apache/james/gatling/jmap/rfc8621/JmapEmail.scala
@@ -241,7 +241,7 @@ object JmapEmail {
            |  ]
            |}""".stripMargin))
       .check(status.is(200))
-      .check(jsonPath("$.methodResponses[0][1].newState").saveAs("newState"))
+      .check(jsonPath("$.methodResponses[0][1].newState").saveAs(oldState))
   }
 }
 
