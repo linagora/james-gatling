4f74f106830a5fd18e5e452f4220b5d4373d57e2
Tung Van TRAN <vttran@linagora.com>
2022-04-13 18:37:01 +0700

    Optimize code - reuse UsersFeederCSVFactory

--- src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/draft/FeederJmapAllSimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/draft/FeederJmapAllSimulation.scala
@@ -2,32 +2,18 @@ package org.apache.james.gatling.simulation.jmap.draft
 
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
-import org.apache.james.gatling.control._
-import org.apache.james.gatling.jmap.draft.RecipientAddress
 import org.apache.james.gatling.jmap.draft.scenari.JmapAllScenario
-import org.apache.james.gatling.simulation.{Configuration, HttpSettings}
+import org.apache.james.gatling.simulation.{Configuration, HttpSettings, UsersFeederCSVFactory}
 
 class FeederJmapAllSimulation extends Simulation {
 
-  private def recordValueToString(recordValue: Any): String = recordValue match {
-    case s: String => s
-    case a: Any => println("Warning: calling toString on a feeder value"); a.toString
-  }
-
-  private val users: Seq[User] = csv("users.csv").readRecords
-    .map(record =>
-      User(
-        username = Username(recordValueToString(record("username"))),
-        password = Password(recordValueToString(record("password")))))
-
   private val scenario = new JmapAllScenario()
-
-  private val recipients: Seq[RecipientAddress] = csv("users.csv").readRecords
-    .map(record =>
-      RecipientAddress(recordValueToString(record("username"))))
+  private val feederFactory: UsersFeederCSVFactory = new UsersFeederCSVFactory().loadUsers
 
   setUp(scenario
-    .generate(UserFeeder.toFeeder(users).circular, Configuration.ScenarioDuration, RecipientFeeder.toFeeder(recipients).random)
+    .generate(feederFactory.userFeeder(),
+      Configuration.ScenarioDuration,
+      feederFactory.recipientFeeder())
     .inject(atOnceUsers(Configuration.UserCount)))
     .protocols(HttpSettings.httpProtocol)
 }
--- src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/draft/PlatformValidationSimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/draft/PlatformValidationSimulation.scala
@@ -1,28 +1,17 @@
 package org.apache.james.gatling.simulation.jmap.draft
 
 import io.gatling.core.Predef._
-import org.apache.james.gatling.control.AuthenticatedUserFeeder.AuthenticatedUserFeederBuilder
-import org.apache.james.gatling.control.{Password, RecipientFeeder, User, UserFeeder, Username}
 import org.apache.james.gatling.jmap.draft.scenari.PlatformValidationScenario
 import org.apache.james.gatling.simulation.Configuration.{InjectionDuration, ScenarioDuration, UserCount}
-import org.apache.james.gatling.simulation.HttpSettings
+import org.apache.james.gatling.simulation.{HttpSettings, UsersFeederCSVFactory}
 
 class PlatformValidationSimulation extends Simulation {
-  private def recordValueToString(recordValue: Any): String = recordValue match {
-    case s: String => s
-    case a: Any => println("Warning: calling toString on a feeder value"); a.toString
-  }
-
-  val authenticatedUsers: Seq[User] = csv("users.csv").readRecords
-    .map(record =>
-      User(
-        username = Username(recordValueToString(record("username"))),
-        password = Password(recordValueToString(record("password")))))
-
-  val feeder: AuthenticatedUserFeederBuilder = UserFeeder.toFeeder(authenticatedUsers)
+  private val feederFactory: UsersFeederCSVFactory = new UsersFeederCSVFactory().loadUsers
 
   setUp(new PlatformValidationScenario(minMessagesInMailbox = 10)
-      .generate(duration = ScenarioDuration, userFeeder = feeder, recipientFeeder = RecipientFeeder.usersToFeeder(authenticatedUsers))
+    .generate(duration = ScenarioDuration,
+      userFeeder = feederFactory.userFeeder(),
+      recipientFeeder = feederFactory.recipientFeeder())
     .inject(rampUsers(UserCount) during InjectionDuration)
     .protocols(HttpSettings.httpProtocol))
 }
