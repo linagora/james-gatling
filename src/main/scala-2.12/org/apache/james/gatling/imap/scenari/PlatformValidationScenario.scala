6df3266a8915660deb0fe09c60774d5a5dcc6fe5
Benoit Tellier <btellier@linagora.com>
2022-04-24 14:48:27 +0700

    ISSUE-133 Refine a relevant IMAP scenario
     -> Try matches percentage of commands observed on production instances
     -> Also uses traffic captures for relevant IMAP commands

--- src/main/scala-2.12/org/apache/james/gatling/imap/scenari/PlatformValidationScenario.scala
+++ src/main/scala-2.12/org/apache/james/gatling/imap/scenari/PlatformValidationScenario.scala
@@ -18,16 +18,16 @@ class PlatformValidationScenario(minWaitDelay: Duration = 2 seconds, maxWaitDela
 
   val initialConnection: ChainBuilder = group("initialConnection")(
     exec(imap("Connect").connect()).exitHereIfFailed
-      .exec(imap("capability1").capability().check(ok))
+      .exec(imap("capability").capability().check(ok))
       .exec(imap("login").login("${username}", "${password}").check(ok))
-      .exec(imap("capability2").capability().check(ok))
+      .exec(imap("capability").capability().check(ok))
       .exec(imap("enableUTF8").enable("UTF8=ACCEPT").check(ok))
       .exec(imap("namespace").namespace().check(ok))
       .exec(imap("list").list("", "*").check(ok))
       .exec(imap("lsub").list("", "*").check(ok))
       .exec(imap("myrights").myRights("INBOX").check(ok))
       .exec(imap("select").select("INBOX").check(ok))
-      .exec(imap("flagResync").fetch(MessageRanges(Range(1, 1000000)), AttributeList("FLAGS"))))
+      .exec(imap("flagResync").fetch(MessageRanges(Range(1, 1000000)), AttributeList("UID", "FLAGS"))))
 
   val closeConnection: ChainBuilder = group("closeConnection")(
     exec(imap("close").close().check(ok))
@@ -40,16 +40,15 @@ class PlatformValidationScenario(minWaitDelay: Duration = 2 seconds, maxWaitDela
       9.0 -> exec(imap("getQuotaRoot").getQuotaRoot("INBOX").check(ok)),
       8.0 -> exec(ImapCommonSteps.receiveEmail), // Higher probability that what is seen in the wild to replace SMTP for message delivery
       8.0 -> exec(imap("noop").noop().check(ok)),
-      6.0 -> exec(imap("fetchFlags").uidFetch(MessageRanges(Range(1, 100000)), AttributeList("UID", "FLAGS")).check(ok)),
-      6.0 -> exec(imap("fetchHeaders").uidFetch(MessageRanges(Last()), AttributeList("UID", "RFC822.SIZE", "FLAGS", "BODY.PEEK[HEADER.FIELDS (From To Cc Bcc Subject Date Message-ID Priority X-Priority References Newsgroups In-Reply-To Content-Type Reply-To)]")).check(ok)),
-      6.0 -> exec(imap("fetchBody").uidFetch(MessageRanges(Last()), AttributeList("UID", "RFC822.SIZE", "BODY.PEEK[]")).check(ok)),
-      2.0 -> exec(imap("searchUid").search(MessageRanges(Range(1, 100000)), null).check(ok)),
+      9.0 -> exec(imap("fetchHeaders").uidFetch(MessageRanges(Last()), AttributeList("UID", "RFC822.SIZE", "FLAGS", "BODY.PEEK[HEADER.FIELDS (From To Cc Bcc Subject Date Message-ID Priority X-Priority References Newsgroups In-Reply-To Content-Type Reply-To)]")).check(ok)),
+      9.0 -> exec(imap("fetchBody").uidFetch(MessageRanges(Last()), AttributeList("UID", "RFC822.SIZE", "BODY.PEEK[]")).check(ok)),
       2.0 -> exec(imap("searchUnseen").search(MessageRanges(Range(1, 100000)), new FlagTerm(new Flags(Flags.Flag.SEEN), false)).check(ok)),
+      2.0 -> exec(imap("searchDeleted").search(MessageRanges(Range(1, 100000)), new FlagTerm(new Flags(Flags.Flag.DELETED), false)).check(ok)),
       2.0 -> exec(imap("select").select("INBOX").check(ok)),
       2.0 -> exec(imap("store").store(MessageRanges(Last()), StoreFlags.add(Silent.Enable(), "\\Seen")).check(ok)),
       1.0 -> exec(imap("list").list("", "*").check(ok, hasFolder("INBOX"))),
       1.0 -> exec(imap("unselect").unselect().check(ok))
-        .exec(imap("select2").select("INBOX").check(ok)),
+        .exec(imap("select").select("INBOX").check(ok)),
       1.0 -> exec(imap("check").check().check(ok)),
       1.0 -> exec(imap("expunge").expunge().check(ok))))
 
