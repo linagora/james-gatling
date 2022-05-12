5abf52fdcb8cf0c03ef0ac69f2d61947d97b62ab
Rene Cordier <rcordier@linagora.com>
2022-05-12 11:46:28 +0700

    Remove default headers in Http protocol definition
    After the upgrade from Gatling 3.0.3 to 3.4.2, it seems those headers are always being injected on top (maybe before they were overrided), thus James was fetching the first one (default) and not the one overrided in the request.
    
    Thus that creates issues with jmap-rfc simulations that rely on the Accept header to use jmap-rfc and not draft.
    
    After scanning the code and usage, it seems safe to me to just remove those.

--- src/test/scala-2.12/org/apache/james/gatling/simulation/HttpSettings.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/HttpSettings.scala
@@ -2,14 +2,11 @@ package org.apache.james.gatling.simulation
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef.http
-import org.apache.james.gatling.jmap.draft.JmapHttp
 
 object HttpSettings {
 
   def httpProtocol = http
     .baseUrl(Configuration.BaseJmapUrl.toString)
-    .acceptHeader(JmapHttp.ACCEPT_JSON_VALUE)
-    .contentTypeHeader(JmapHttp.CONTENT_TYPE_JSON_VALUE)
     .wsBaseUrl(Configuration.BaseWsUrl)
 
 }
