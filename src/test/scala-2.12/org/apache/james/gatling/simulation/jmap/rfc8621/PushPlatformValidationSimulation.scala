f92a0674a695157ad05209125ec7735f66dc6095
Tung Tran <vttran@linagora.com>
2023-07-10 18:00:22 +0700

    Support MAX_DURATION to force your run to terminate based on a duration limit
    - even if some virtual users are still running
    - support get HTML result  when running hanging at 99%

--- src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/rfc8621/PushPlatformValidationSimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/jmap/rfc8621/PushPlatformValidationSimulation.scala
@@ -3,7 +3,7 @@ package org.apache.james.gatling.simulation.jmap.rfc8621
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
 import org.apache.james.gatling.jmap.rfc8621.scenari.PushPlatformValidationScenario
-import org.apache.james.gatling.simulation.Configuration.{InjectionDuration, ScenarioDuration, UserCount}
+import org.apache.james.gatling.simulation.Configuration.{InjectionDuration, MaxDuration, ScenarioDuration, UserCount}
 import org.apache.james.gatling.simulation.{HttpSettings, UsersFeederCSVFactory}
 
 class PushPlatformValidationSimulation extends Simulation {
@@ -15,4 +15,5 @@ class PushPlatformValidationSimulation extends Simulation {
       recipientFeeder = feederFactory.recipientFeeder())
     .inject(rampUsers(UserCount) during InjectionDuration)
     .protocols(HttpSettings.httpProtocol))
+    .maxDuration(MaxDuration)
 }
