de6962e0258a57ed51181c202b79b6c735987294
Rene Cordier <rcordier@linagora.com>
2024-04-16 16:39:48 +0700

    Remove jmap draft from james-gatling

--- src/it/scala-2.13/org/apache/james/gatling/jmap/draft/JmapAllScenarioIT.scala
+++
@@ -1,9 +0,0 @@
-package org.apache.james.gatling.jmap.draft
-
-import org.apache.james.gatling.jmap.draft.scenari.JmapAllScenario
-
-import scala.concurrent.duration._
-
-class JmapAllScenarioIT extends JmapIT {
-  scenario((userFeederBuilder, recipientFeederBuilder) => new JmapAllScenario().generate(userFeederBuilder, 10.seconds, recipientFeederBuilder))
-}
--- src/it/scala-2.13/org/apache/james/gatling/jmap/draft/JmapAuthenticationIT.scala
+++
@@ -1,7 +0,0 @@
-package org.apache.james.gatling.jmap.draft
-
-import org.apache.james.gatling.jmap.draft.scenari.JmapAuthenticationScenario
-
-class JmapAuthenticationIT extends JmapIT {
-  scenario((userFeederBuilder, _) => new JmapAuthenticationScenario().generate(userFeederBuilder))
-}
